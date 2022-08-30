4fa3e17a7cf465fb785f7a7d2838280d04009c8a
Samyak Jain <2130153085@ipec.org.in>
2022-08-30 21:14:22 +0530

    added bootstrap css and js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 
 import React, {useState, useEffect  } from 'react'
+import Navbar from './Components/Navbar';
 
 function App() {
 
   return (
     <>
+    <Navbar/>
     Samyak Jain is here!
+
     </>
   );
 }
